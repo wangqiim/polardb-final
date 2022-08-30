ce9b4ca66480984a906ba352744373acaaca3936
lmjhh <576788582@qq.com>
2022-08-30 13:59:34 +0800

    add rwlock

--- inc/index/MemIndex.h
+++ inc/index/MemIndex.h
@@ -43,7 +43,7 @@ struct Str128Hash {
 };
 
 std::mutex index_mtx;
-
+pthread_rwlock_t rwlock[50];
 uint32_t thread_pos[50];
 // static emhash7::HashMap<uint64_t, uint32_t> pk[HASH_MAP_COUNT];
 // static emhash7::HashMap<UserId, uint32_t, UserIdHash> uk[HASH_MAP_COUNT];
@@ -56,16 +56,18 @@ static std::unordered_map<uint64_t, std::vector<uint32_t>> sk[HASH_MAP_COUNT];
 static void initIndex() {
   spdlog::info("Init Index Begin");
   memset(thread_pos, 0, sizeof(thread_pos));
-  // for (int i = 0; i < HASH_MAP_COUNT; i++) {
+
+  for (int i = 0; i < HASH_MAP_COUNT; i++) {
+    pthread_rwlock_init(&rwlock[i], NULL);
   //   pk[i].reserve(4000000);
   //   uk[i].reserve(4000000);
   //   sk[i].reserve(4000000);
-  // }
+  }
   spdlog::info("Init Index End");
 }
 
 static void insert(const char *tuple, size_t len, uint8_t tid) {
-  std::lock_guard<std::mutex> guard(index_mtx);
+    pthread_rwlock_wrlock(&rwlock[tid]);
     uint32_t pos = thread_pos[tid] + PER_THREAD_MAX_WRITE * tid;
     pk[tid].insert(std::pair<uint64_t, uint32_t>(*(uint64_t *)tuple, pos));
     uk[tid].insert(std::pair<Str128, uint32_t>(Str128(tuple + 8), pos));
@@ -77,36 +79,51 @@ static void insert(const char *tuple, size_t len, uint8_t tid) {
         sk[tid].insert(std::pair<uint64_t, std::vector<uint32_t>>(*(uint64_t *)(tuple + 264), {pos}));
     }
     thread_pos[tid]++;
+    pthread_rwlock_unlock(&rwlock[tid]);
 } 
 
 static std::vector<uint32_t> getPosFromKey(int32_t where_column, const void *column_key) {
-  std::lock_guard<std::mutex> guard(index_mtx);
   std::vector<uint32_t> result;
   if (where_column == Id) {
     for (int i = 0; i < HASH_MAP_COUNT; i++) {
+      bool isFind = false;
+      pthread_rwlock_rdlock(&rwlock[i]);
       auto it = pk[i].find(*(uint64_t *)(column_key));
       if (it != pk[i].end()) {
-        return {it -> second};
-      } 
+        isFind = true;
+      }
+      pthread_rwlock_unlock(&rwlock[i]);
+      if (isFind) return {it -> second};
     }
   }
   if (where_column == Userid) {
     for (int i = 0; i < HASH_MAP_COUNT; i++) {
+      bool isFind = false;
+      pthread_rwlock_rdlock(&rwlock[i]);
       auto it = uk[i].find(Str128((char *)column_key));
       if (it != uk[i].end()) {
-        return {it -> second};
+        isFind = true;
       } 
+      pthread_rwlock_unlock(&rwlock[i]);
+      if (isFind) return {it -> second};
     }    
   }
   if (where_column == Salary) {
     for (int i = 0; i < HASH_MAP_COUNT; i++) {
+      bool isFind = false;
+      pthread_rwlock_rdlock(&rwlock[i]);
       auto it = sk[i].find(*(uint64_t *)((char *)column_key));
       if (it != sk[i].end()) {
+        isFind = true;
+      }
+      pthread_rwlock_unlock(&rwlock[i]);
+      if (isFind) {
         for (int j = 0; j < it -> second.size(); j++) {
           result.push_back(it -> second[j]);
         }
       }
     }
   }
+
   return result;
 }
