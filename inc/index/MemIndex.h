2c945e851fd1030f66aadd1b78876c2436715f08
lmjhh <576788582@qq.com>
2022-08-31 01:28:13 +0800

    memindex don't use lock

--- inc/index/MemIndex.h
+++ inc/index/MemIndex.h
@@ -69,7 +69,7 @@ static void initIndex() {
 // 1. recovery时调用
 // 2. write插入数据时调用
 static void insert(const char *tuple, size_t len, uint8_t tid) {
-    pthread_rwlock_wrlock(&rwlock[tid]);
+    // pthread_rwlock_wrlock(&rwlock[tid]);
     uint32_t pos = thread_pos[tid] + PER_THREAD_MAX_WRITE * tid;
     pk[tid].insert(std::pair<uint64_t, uint32_t>(*(uint64_t *)tuple, pos));
     uk[tid].insert(std::pair<Str128, uint32_t>(Str128(tuple + 8), pos));
@@ -81,7 +81,7 @@ static void insert(const char *tuple, size_t len, uint8_t tid) {
         sk[tid].insert(std::pair<uint64_t, std::vector<uint32_t>>(*(uint64_t *)(tuple + 264), {pos}));
     }
     thread_pos[tid]++;
-    pthread_rwlock_unlock(&rwlock[tid]);
+    // pthread_rwlock_unlock(&rwlock[tid]);
 } 
 
 static std::vector<uint32_t> getPosFromKey(int32_t where_column, const void *column_key) {
@@ -89,31 +89,31 @@ static std::vector<uint32_t> getPosFromKey(int32_t where_column, const void *col
   if (where_column == Id) {
     for (int i = 0; i < HASH_MAP_COUNT; i++) {
       bool isFind = false;
-      pthread_rwlock_rdlock(&rwlock[i]);
+      // pthread_rwlock_rdlock(&rwlock[i]);
       auto it = pk[i].find(*(uint64_t *)(column_key));
       if (it != pk[i].end()) {
         isFind = true;
       }
       if (isFind) result.push_back(it->second);
-      pthread_rwlock_unlock(&rwlock[i]);
+      // pthread_rwlock_unlock(&rwlock[i]);
     }
   }
   if (where_column == Userid) {
     for (int i = 0; i < HASH_MAP_COUNT; i++) {
       bool isFind = false;
-      pthread_rwlock_rdlock(&rwlock[i]);
+      // pthread_rwlock_rdlock(&rwlock[i]);
       auto it = uk[i].find(Str128((char *)column_key));
       if (it != uk[i].end()) {
         isFind = true;
       } 
       if (isFind) result.push_back(it->second);
-      pthread_rwlock_unlock(&rwlock[i]);
+      // pthread_rwlock_unlock(&rwlock[i]);
     }    
   }
   if (where_column == Salary) {
     for (int i = 0; i < HASH_MAP_COUNT; i++) {
       bool isFind = false;
-      pthread_rwlock_rdlock(&rwlock[i]);
+      // pthread_rwlock_rdlock(&rwlock[i]);
       auto it = sk[i].find(*(uint64_t *)((char *)column_key));
       if (it != sk[i].end()) {
         isFind = true;
@@ -123,7 +123,7 @@ static std::vector<uint32_t> getPosFromKey(int32_t where_column, const void *col
           result.push_back(it -> second[j]);
         }
       }
-      pthread_rwlock_unlock(&rwlock[i]);
+      // pthread_rwlock_unlock(&rwlock[i]);
     }
   }
 
