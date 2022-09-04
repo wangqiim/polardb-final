28faf8c7b257d612fafc4b2db200e5e4c2bd3921
wangqiim <wangqiim@163.com>
2022-09-04 19:26:35 +0800

    fix: Abnormal getTid

--- inc/NvmDB.h
+++ inc/NvmDB.h
@@ -41,9 +41,13 @@ static void Put(const char *tuple, size_t len){
 static std::atomic<uint8_t> getTid(0);
 static size_t Get(int32_t select_column,
           int32_t where_column, const void *column_key, size_t column_key_len, void *res, bool is_local){
-    static thread_local uint8_t tid = getTid++;
-    if (tid >= 50) {
-      spdlog::error("[Get] tid overflow, tid = {}", tid);
+    static thread_local uint8_t tid = 0;
+    if (is_local == true && tid == 0) { // socket_server 也会调用该函数，防止tid溢出
+      getTid++;
+      if (tid >= 50) {
+        spdlog::error("[Get] tid overflow, tid = {}", tid);
+        exit(1);
+      }
     }
     static thread_local int local_read_count = 0;
     static thread_local int remote_read_count = 0;
