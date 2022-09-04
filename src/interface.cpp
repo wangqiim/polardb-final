7307a09c9a7d4279cfd641723460e5dd5ed1678c
lmjhh <576788582@qq.com>
2022-09-04 16:02:14 +0800

    change log info

--- src/interface.cpp
+++ src/interface.cpp
@@ -17,7 +17,7 @@ size_t engine_read( void *ctx, int32_t select_column,
 
 void* engine_init(const char* host_info, const char* const* peer_host_info, size_t peer_host_info_num,
                   const char* aep_dir, const char* disk_dir) {
-    spdlog::set_level(spdlog::level::debug);
+    spdlog::set_level(spdlog::level::info);
     call_init_num++;
     spdlog::info("version [without remoteGet backoff], call engine_init times = {}", call_init_num);
     initNvmDB(host_info, peer_host_info, peer_host_info_num, aep_dir, disk_dir);
