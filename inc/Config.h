ff8b0f2122d413cb89e0545da27b56d39a168cde
lmjhh <576788582@qq.com>
2022-09-07 14:10:01 +0800

    skshard 8096

--- inc/Config.h
+++ inc/Config.h
@@ -14,7 +14,7 @@ const uint64_t COMMIT_FLAG_SIZE = 4;
 const uint64_t PER_THREAD_MAX_WRITE = 5e6; // 有的线程会写超过400W，这里取500万
 //哈希索引分块
 const uint64_t UK_HASH_MAP_SHARD = 1<<13;
-const uint64_t SK_HASH_MAP_SHARD = 1<<7;
+const uint64_t SK_HASH_MAP_SHARD = 1<<13;
 const uint64_t MAX_HASH_MAP_SHARD = 1<<13; // 该值必须足够大 (预分配读写锁数组)
 //NMV文件数
 const uint64_t PMEM_FILE_COUNT = 50;
