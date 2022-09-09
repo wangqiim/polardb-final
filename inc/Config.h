cca703f3228a1875791f91e7755c209fdb91dbee
lmjhh <576788582@qq.com>
2022-09-10 00:52:35 +0800

    use 1<<16

--- inc/Config.h
+++ inc/Config.h
@@ -13,8 +13,8 @@ const uint64_t TOTAL_WRITE_NUM = 2e8; // 复赛单节点总写入2亿数据
 const uint64_t COMMIT_FLAG_SIZE = 4;
 const uint64_t PER_THREAD_MAX_WRITE = 5e6; // 有的线程会写超过400W，这里取500万
 //哈希索引分块
-const uint64_t UK_HASH_MAP_SHARD = 1<<20;
-const uint64_t SK_HASH_MAP_SHARD = 1<<20;
+const uint64_t UK_HASH_MAP_SHARD = 1<<16;
+const uint64_t SK_HASH_MAP_SHARD = 1<<16;
 //NMV文件数
 const uint64_t PMEM_FILE_COUNT = 50;
 
