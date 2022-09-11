6f09845d2cbc092d3b891e0f4407c92ed3918f20
lmjhh <576788582@qq.com>
2022-09-11 13:04:46 +0800

    test

--- inc/Config.h
+++ inc/Config.h
@@ -11,7 +11,7 @@ const uint64_t MEM_RECORD_SIZE = 16;
 
 //表空间大小
 const uint64_t TABLE_SIZE = 50000005;
-const uint64_t TOTAL_WRITE_NUM = 2e8; // 复赛单节点总写入2亿数据
+const uint64_t TOTAL_WRITE_NUM = 1 << 28; // 复赛单节点总写入2亿数据
 
 const uint64_t COMMIT_FLAG_SIZE = 4;
 const uint64_t PER_THREAD_MAX_WRITE = 5e6; // 有的线程会写超过400W，这里取500万
