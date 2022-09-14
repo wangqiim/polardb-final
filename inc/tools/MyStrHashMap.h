24090469e1e84eb345ab5a904d1bd15575489ca0
wangqiim <wangqiim@163.com>
2022-09-14 14:54:03 +0800

    op: sk index memory usage

--- inc/tools/MyStrHashMap.h
+++ inc/tools/MyStrHashMap.h
@@ -12,7 +12,6 @@ struct alignas(64) MyStr256Head
 
 struct alignas(8) MyStrHead {
     uint32_t value = 0;
-    std::vector<uint32_t> *ptr = nullptr;
 };
 
 
@@ -20,6 +19,15 @@ class MyStringHashMap {
   public:
   MyStringHashMap() {
     hash_table = new MyStrHead[hashSize];
+    pmem_record_num_ = 0;
+    uint64_t mmap_size = TOTAL_WRITE_NUM * 4;
+    int is_pmem;
+    size_t mapped_len;
+    if ( (pmem_addr_ = (char *)pmem_map_file("/mnt/aep/skindex", mmap_size, PMEM_FILE_CREATE,
+                                                  0666, &mapped_len, &is_pmem)) == NULL ) {
+      spdlog::error("[MyStringHashMap] pmem_map_file");
+    }
+    pmem_memset_nodrain(pmem_addr_, 0, mmap_size);
   }
 
   ~MyStringHashMap() {
@@ -31,9 +39,13 @@ class MyStringHashMap {
     if (hash_table[pos].value == 0) return;
     else {
       ans.push_back(hash_table[pos].value - 1);
-      if (hash_table[pos].ptr != nullptr) {
-        for (uint32_t i = 0; i < hash_table[pos].ptr->size(); i++) {
-          ans.push_back(hash_table[pos].ptr->at(i));
+      if (pmem_record_num_ != 0) {
+        std::lock_guard<std::mutex> guard(mtx);
+        for (uint64_t i = 0; i < pmem_record_num_; i++) {
+          uint32_t temp = *(uint32_t *)(pmem_addr_ + i * 4);
+          if (key == temp) {
+            ans.push_back(temp);
+          }
         }
       }
     }
@@ -44,12 +56,9 @@ class MyStringHashMap {
     if (hash_table[pos].value == 0) {
       hash_table[pos].value = value + 1;
     } else {
-      mtx.lock();
-      if (hash_table[pos].ptr == nullptr) {
-        hash_table[pos].ptr = new std::vector<uint32_t>();
-      }
-      hash_table[pos].ptr->push_back(value);
-      mtx.unlock();
+      std::lock_guard<std::mutex> guard(mtx);
+      pmem_memcpy_nodrain(pmem_addr_ + pmem_record_num_ * 4, &value, 4);
+      pmem_record_num_++;
     }
   }
 
@@ -60,7 +69,10 @@ class MyStringHashMap {
   private:
     MyStrHead *hash_table;
     const uint32_t hashSize = 1<<30;
+    
     std::mutex mtx;
+    char *pmem_addr_;
+    uint64_t pmem_record_num_;
 };
 
 class MyString256HashMap {
