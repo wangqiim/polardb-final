37ac909a8ae326f86933a9b9ba69b06794a22d94
lmjhh <576788582@qq.com>
2022-09-28 16:23:27 +0800

    add hack

--- inc/store/NvmStoreV3.h
+++ inc/store/NvmStoreV3.h
@@ -6,10 +6,9 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/mman.h>
-#include <set>
 
 #include "Config.h"
-#include "./index/MemIndexV2.h"
+#include "./index/MemIndex.h"
 
 class User
 {
@@ -22,15 +21,13 @@ public:
 
 struct PmemBlockMeta {
     char *address = nullptr;
-    uint32_t offset = 0;
-}PBM, PRBM;
+    uint64_t offset = 0;
+}PBM[PMEM_FILE_COUNT], PIDM[PMEM_FILE_COUNT];
 
 struct MemBlockMeta {
     char *address = nullptr;
-}MBM;
-
-static std::atomic<uint32_t> write_count(0);
-uint32_t mod = (1<<28);
+    uint64_t offset = 0;
+}MBM[PMEM_FILE_COUNT];
 
 static inline void print_time(struct timeval t1, struct timeval t2) {
   double timeuse = (t2.tv_sec - t1.tv_sec) + (double)(t2.tv_usec - t1.tv_usec)/1000000.0;
@@ -39,134 +36,161 @@ static inline void print_time(struct timeval t1, struct timeval t2) {
 
 static void recovery();
 
-static void create_mem_metal(MemBlockMeta &meta, std::string path, size_t mmap_size) {
-  bool is_create = true;
-  int fd;
-  if (access(path.c_str(), F_OK) == 0) {
-    is_create = false;
-    fd = open(path.c_str(), O_RDWR);
-    lseek(fd, mmap_size - 1, SEEK_SET);
-  } else {
-    fd = open(path.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0644);
-    lseek(fd, mmap_size - 1, SEEK_SET);
-    if (write(fd, "0", 1) < 0) {
-      spdlog::error("write error");
-    }
+static void Store_Sync() {
+  const size_t mmap_size = MEM_RECORD_SIZE * PER_THREAD_MAX_WRITE + COMMIT_FLAG_SIZE;
+  for (size_t i = 0; i < PMEM_FILE_COUNT; i++) {
+    msync(MBM[i].address - COMMIT_FLAG_SIZE, mmap_size, MS_ASYNC);
   }
-  meta.address = (char *)mmap(0, mmap_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
-  if (is_create) {
-    memset(meta.address, 0, mmap_size);
-  }
-  close(fd);
 }
 
-static void create_pmem_metal(PmemBlockMeta &meta, std::string path, size_t mmap_size) {
+static void create_metal(std::string path, int i, bool is_pmem, bool is_id) {
+  if (is_pmem) {
+    path = path + "pmem" + std::to_string(i) + ".pool";
+  } else {
+    path = path + "mem" + std::to_string(i) + ".pool";
+  }
   bool is_create = true;
+  int fd;
+  size_t mmap_size = MEM_RECORD_SIZE * PER_THREAD_MAX_WRITE + COMMIT_FLAG_SIZE;
   if (access(path.c_str(), F_OK) == 0) {
     is_create = false;
+    if(!is_pmem) {
+      fd = open(path.c_str(), O_RDWR);
+      lseek(fd, mmap_size - 1, SEEK_SET);
+    }
+  } else {
+    if(!is_pmem) {
+      fd = open(path.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0644);
+      lseek(fd, mmap_size - 1, SEEK_SET);
+      if (write(fd, "0", 1) < 0) {
+        spdlog::error("write error");
+      }
+    }
   }
-  int is_pmem;
-  size_t mapped_len;
-  /* create a pmem file and memory map it */
-  if ( (meta.address = (char *)pmem_map_file(path.c_str(), mmap_size, PMEM_FILE_CREATE,
-                                                 0666, &mapped_len, &is_pmem)) == NULL ) {
-    spdlog::error("pmem_map_file");
+  if (is_pmem) {
+    int is_pmem;
+    size_t mapped_len;
+    if (is_id) {
+      mmap_size = 8 * PER_THREAD_MAX_WRITE;
+      /* create a pmem file and memory map it */
+      if ((PIDM[i].address = (char *) pmem_map_file(path.c_str(), mmap_size, PMEM_FILE_CREATE,
+                                                    0666, &mapped_len, &is_pmem)) == NULL) {
+        spdlog::error("pmem_map_file");
+      }
+    } else {
+      mmap_size = PMEM_RECORD_SIZE * PER_THREAD_MAX_WRITE;
+      /* create a pmem file and memory map it */
+      if ((PBM[i].address = (char *) pmem_map_file(path.c_str(), mmap_size, PMEM_FILE_CREATE,
+                                                   0666, &mapped_len, &is_pmem)) == NULL) {
+        spdlog::error("pmem_map_file");
+      }
+    }
+  } else {
+    MBM[i].address = (char *)mmap(0, mmap_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+    MBM[i].address += COMMIT_FLAG_SIZE;
   }
   if (is_create) {
-    pmem_memset_nodrain(meta.address, 0, mmap_size);
+    if (is_pmem) {
+      pmem_memset_nodrain(PBM[i].address, 0, mmap_size);
+    } else {
+      memset(MBM[i].address - COMMIT_FLAG_SIZE, 0, mmap_size);
+      close(fd);
+    }
   }
 }
 
 static void initStore(const char* aep_dir,  const char* disk_dir) {
   spdlog::info("get sys pmem dir: {} ssd dir: {}", aep_dir, disk_dir);
-  create_mem_metal(MBM, std::string(disk_dir) + "mem.pool", 9 * NVSTORE_WRITE_NUM + COMMIT_FLAG_SIZE);
-  create_pmem_metal(PBM, std::string(aep_dir) + "pmem.pool", PMEM_RECORD_SIZE * NVSTORE_WRITE_NUM);
-  create_pmem_metal(PRBM ,std::string(aep_dir) + "rpmem.pool", 8 * 100000 + COMMIT_FLAG_SIZE);
-  MBM.address += COMMIT_FLAG_SIZE + 1; //用一个字节表示d
-  PRBM.address += COMMIT_FLAG_SIZE;
+  for (uint64_t i = 0; i < PMEM_FILE_COUNT; i++) {
+    create_metal(std::string(aep_dir), i, true, false);
+    create_metal(std::string(disk_dir), i, false, false);
+    create_metal(std::string(aep_dir) + "id", i, true, true);
+  }
   recovery();
   spdlog::info("Store Init End");
 }
 
-std::mutex rid_mutex;
 static void writeTuple(const char *tuple, __attribute__((unused)) size_t len, uint8_t tid) {
-  uint64_t id = *(uint64_t *)tuple;
-  uint64_t index = id % mod;
-  if (id >= 8e8) { //单独存储原始值
-    //互斥存储
-    rid_mutex.lock();
-    pmem_memcpy_nodrain(PRBM.address + PRBM.offset * 8, &id, 8);
-    PRBM.offset++;
-    pmem_memcpy_nodrain(PRBM.address - 4, &PRBM.offset, 4);
-    rid_mutex.unlock();
+  if (*(uint64_t *)tuple < 8e8) {
+    memcpy(MBM[tid].address + MBM[tid].offset, tuple, 4);
+  } else {
+    pmem_memcpy_nodrain(PIDM[tid].address + PIDM[tid].offset, tuple, 8);
   }
-
-  //user_id 和 name
-  pmem_memcpy_nodrain(PBM.address + index * PMEM_RECORD_SIZE, tuple + 8, 256);
+  memcpy(MBM[tid].address + MBM[tid].offset + 4, tuple + 264, 8);
+  pmem_memcpy_nodrain(PBM[tid].address + PBM[tid].offset, tuple + 8, 256);
+  uint32_t pos = (PBM[tid].offset / PMEM_RECORD_SIZE) + 1;
+  memcpy(MBM[tid].address - 4, &pos, 4);
   pmem_drain();
-
-  //salary 和 flag
-  memcpy(MBM.address + index * 9, tuple + 264, 8);
-  uint8_t flag = 1;
-  memcpy(MBM.address + index * 9 + 8, &flag, 1);
-
-  write_count++;
-  memcpy(MBM.address - 4, &write_count, 4);
+  PBM[tid].offset += PMEM_RECORD_SIZE;
+  MBM[tid].offset += MEM_RECORD_SIZE;
+  PIDM[tid].offset += 8;
 }
 
-static void readColumFromPos(int32_t select_column, uint64_t id, void *res) {
-  uint64_t index = id % mod;
+static void readColumFromPos(int32_t select_column, uint32_t pos, void *res) {
+  uint8_t tid = pos / PER_THREAD_MAX_WRITE;
+  uint64_t offset = (pos % PER_THREAD_MAX_WRITE);
   if (select_column == Id) {
+    uint64_t id = *(uint32_t *)(MBM[tid].address + offset * MEM_RECORD_SIZE);
+    if (id > 0) {
+      memcpy(res, &id, 8);
+      return;
+    }
+    id = *(uint64_t *)(PIDM[tid].address + offset * 8);
     memcpy(res, &id, 8);
     return;
   }
   if (select_column == Userid) {
-    memcpy(res, PBM.address + index * PMEM_RECORD_SIZE, 128);
+    memcpy(res, PBM[tid].address + offset * PMEM_RECORD_SIZE, 128);
     return;
   }
   if (select_column == Name) {
-    memcpy(res, PBM.address + index * PMEM_RECORD_SIZE + 128, 128);
+    memcpy(res, PBM[tid].address + offset * PMEM_RECORD_SIZE + 128, 128);
     return;
   }
   if (select_column == Salary) {
-    memcpy(res, MBM.address + index * 9, 8);
+    memcpy(res, MBM[tid].address + offset * MEM_RECORD_SIZE + 4, 8);
     return;
   }
 }
 
+static char *GetUserIdByPos(uint32_t pos) {
+  uint8_t tid = pos / PER_THREAD_MAX_WRITE;
+  uint64_t offset = (pos % PER_THREAD_MAX_WRITE);
+  return PBM[tid].address + offset * PMEM_RECORD_SIZE;
+}
+
 // build index
-static void recovery() {
-  uint64_t recovery_cnt = 0;
-  //总数
-  uint32_t commit_cnt = *(uint32_t *)(MBM.address - 4);
-  //恢复随机id的数量
-  uint32_t rid_cont = *(uint32_t *)(PRBM.address - 4);
-  std::set<uint64_t> rid_set;
-  for (uint32_t i = 0; i < rid_cont; i++) {
-    unsigned char tuple[RECORD_SIZE];
-    uint64_t id = *(uint64_t *)(PRBM.address + i * 8);
-    memcpy(tuple, &id, 8);
-    memcpy(tuple + 8, PBM.address + (id % mod) * PMEM_RECORD_SIZE, 256);
-    memcpy(tuple + 264, MBM.address + (id % mod) * 9, 8);
-    insert((const char *)tuple, RECORD_SIZE, 0);
-    rid_set.insert(id % mod);
-    PRBM.offset++;
-    recovery_cnt++;
-  }
-  spdlog::info("current count {} need recovery {} tuple", recovery_cnt, commit_cnt);
-  for (uint64_t index = 0; index < NVSTORE_WRITE_NUM; index++) {
-    //空数据的跳过
-    if (rid_set.find(index) == rid_set.end() && (*(uint8_t *)(MBM.address + index * 9 + 8)) == 1) {
+static void recovery() {;
+  for (uint64_t i = 0; i < PMEM_FILE_COUNT; i++) {
+    uint32_t commit_cnt = *(uint32_t *)(MBM[i].address - 4);
+    for (uint64_t j = 0; j < commit_cnt; j++) {
       unsigned char tuple[RECORD_SIZE];
-      memcpy(tuple, &index, 8);
-      memcpy(tuple + 8, PBM.address + index * PMEM_RECORD_SIZE, 256);
-      memcpy(tuple + 264, MBM.address + index * 9, 8);
-      insert((const char *) tuple, RECORD_SIZE, 0);
-      recovery_cnt++;
-      if (recovery_cnt >= commit_cnt) break;
+
+      uint64_t id = *(uint32_t *)(MBM[i].address + MBM[i].offset);
+      if (id > 0) {
+        memcpy(tuple, &id, 8);
+      } else {
+        id = *(uint64_t *)(PIDM[i].address + PIDM[i].offset);
+        memcpy(tuple, &id, 8);
+      }
+
+      memcpy(tuple + 8, PBM[i].address + PBM[i].offset, 128);
+      memcpy(tuple + 136, PBM[i].address + PBM[i].offset + 128, 128);
+      memcpy(tuple + 264, MBM[i].address + MBM[i].offset + 4, 8);
+      insert((const char *)tuple, RECORD_SIZE, i);
+      PBM[i].offset += PMEM_RECORD_SIZE;
+      MBM[i].offset += MEM_RECORD_SIZE;
+      PIDM[i].offset += 8;
     }
+    recovery_cnt += PBM[i].offset/PMEM_RECORD_SIZE;
   }
   spdlog::info("recovery {} tuples", recovery_cnt);
   spdlog::info("RECOVERY END");
 }
 
+static void store_stat() {
+  spdlog::info("-------------store stat-----------------------");
+  for (uint32_t i = 0; i < PMEM_FILE_COUNT; i++) {
+    spdlog::info("write tid={}, write count = {}", i, PBM[i].offset/PMEM_RECORD_SIZE);
+  }
+}
