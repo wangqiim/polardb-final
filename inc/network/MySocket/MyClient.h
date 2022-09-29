677142fd033ec78b1682957980b0ade6cfad1a2f
lmjhh <576788582@qq.com>
2022-09-29 21:01:52 +0800

    maby fix bug

--- inc/network/MySocket/MyClient.h
+++ inc/network/MySocket/MyClient.h
@@ -122,12 +122,13 @@ int client_salary_send(char *salary, int tid, int server) {
     return -1;
   }
 //  const int need_send_size = salary_page_cnt * 8;
-//  char send_buf[need_send_size];
+  char send_buf[send_salary_page_size];
 //  for (uint32_t i = 0; i < salary_page_cnt; ++i) {
 //    memcpy(send_buf + i * 8, salary + i * MEM_RECORD_SIZE, 4); // 前4字节放id，后4字节放salary
 //    memcpy(send_buf + i * 8 + 4, salary + i * MEM_RECORD_SIZE + 4, 4); // 前4字节放id，后4字节放salary
 //  }
-  ssize_t send_bytes = send(write_clients[server][tid], salary, send_salary_page_size, 0);
+  memcpy(send_buf, salary, send_salary_page_size);
+  ssize_t send_bytes = send(write_clients[server][tid], send_buf, send_salary_page_size, 0);
   if (send_bytes <= 0) {
     if (send_bytes == 0) { // 远端关闭 eof
       spdlog::debug("[client_salary_send] read eof!");
