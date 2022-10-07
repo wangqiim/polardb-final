a30761625e0367974ddcdd24d993af8c2113f857
wangqiim <wangqiim@163.com>
2022-10-07 13:01:06 +0800

    use uk lock

--- inc/index/MemIndex.h
+++ inc/index/MemIndex.h
@@ -171,12 +171,12 @@ static std::vector<uint32_t> getPosFromKey(int32_t where_column, const void *col
     }
     
     uint32_t uk_shard = shardhashfn(uid.hashCode);
-    // pthread_rwlock_rdlock(&rwlock[1][uk_shard]);
+    pthread_rwlock_rdlock(&uk_rwlock[uk_shard]);
     auto it = uk[uk_shard].find(uid);
     if (it != uk[uk_shard].end()) {
       result.push_back(it->second);
     } 
-    // pthread_rwlock_unlock(&rwlock[1][uk_shard]);
+    pthread_rwlock_unlock(&uk_rwlock[uk_shard]);
   }
   if (where_column == Salary) {
     uint64_t salary = *(uint64_t *)((char *)column_key);
